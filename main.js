f6c023f869e5eb17265f260b17eaecb0ef2b7fc6
ashtamarajbinde <amrajbinde@gmail.com>
2023-05-25 18:02:46 -0600

    'add'

---
+++ main.js
@@ -0,0 +1,48 @@
+// navbar scroll
+
+let nav= document.querySelector(".navbar");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+
+    }
+}
+
+// navbar hide
+
+let navbar= document.querySelectorAll(".nav-link");
+let navCollapse=document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(added){
+    added.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+// owl carousel
+
+
+$(document).ready(function(){
+    $(".client-slider-section").owlCarousel({
+        items:4,
+        loop:true,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:4
+            },
+            1300:{
+                items:6
+            }
+        }
+    });
+})
